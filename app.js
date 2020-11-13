6cdabb5f0a81e79310b0a62ead561df89097fad7
encore-tvm <74394472+encore-tvm@users.noreply.github.com>
2020-11-13 20:46:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ const app = express();
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: 'mysqldb.citiucgqt1bu.ap-southeast-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'core2000',
+  database: 'studentdb'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('DB Connected!');
+});
+
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Sunil John application." });
 });
 
 // set port, listen for requests
